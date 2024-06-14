d43019f417158e58a243359ed832827f6a5c9fdd
Abin <abin>
2024-06-14 18:43:40 +0530

    modal added using ant design

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,19 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Modal } from 'antd';
+
 
 const Body = () => {
+    const [isModalOpen1, setIsModalOpen1] = useState(false);
+    const [isModalOpen2, setIsModalOpen2] = useState(false);
+    const [isModalOpen3, setIsModalOpen3] = useState(false);
+
+
+    const handleOk = () => {
+        setIsModalOpen1(false);
+    };
     return (
         <div className='w-full p-5'>
             <div className=' flex justify-end gap-5'>
-                <button className="bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 h-12 rounded-2xl">
+                <button
+                    onClick={() => {
+                        setIsModalOpen1(true)
+                    }}
+                    className="bg-yellow-500 hover:bg-[#003F62] text-white font-bold py-2 px-4  rounded-2xl transition-colors duration-300"
+                >
                     Add category
                 </button>
-                <button className="bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">
+                <button
+                    onClick={() => {
+                        setIsModalOpen2(true)
+                    }} className="bg-yellow-500 hover:bg-[#003F62] text-white font-bold py-2 px-4 rounded-2xl transition-colors duration-300">
                     Add sub category
                 </button>
-                <button className="bg-yellow-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-2xl">
+                <button
+                    onClick={() => {
+                        setIsModalOpen3(true)
+                    }} className="bg-yellow-500 hover:bg-[#003F62] text-white font-bold py-2 px-4 rounded-2xl transition-colors duration-300">
                     Add product
                 </button>
             </div>
+
+            <Modal title="Add category" open={isModalOpen1} onOk={() => {
+                setIsModalOpen1(false)
+            }} onCancel={() => {
+                setIsModalOpen1(false)
+            }}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+
+            <Modal title="Add sub category" open={isModalOpen2} onOk={() => {
+                setIsModalOpen2(false)
+            }} onCancel={() => {
+                setIsModalOpen2(false)
+            }}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+
+            <Modal title="Add product" open={isModalOpen3} onOk={() => {
+                setIsModalOpen3(false)
+            }} onCancel={() => {
+                setIsModalOpen3(false)
+            }}>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
         </div>
     )
 }
