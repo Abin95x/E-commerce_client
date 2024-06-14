c8f3c9b13eee3c7bc2524c99a94fe4bc5d9c64ee
Abin <abin>
2024-06-14 18:42:30 +0530

    made responsive header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,6 @@ import { toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from 'react-router-dom';
 
-
 const Header = () => {
     const navigate = useNavigate();
     const handleLogout = async () => {
@@ -17,22 +16,28 @@ const Header = () => {
     };
     return (
         <div className='w-full h-20 bg-[#003F62] flex items-center'>
-            <div className=' w-full flex justify-around items-center'>
-                <div className='relative'>
-                    <input type="text" className='bg-white rounded-xl h-10' />
-                    <button className='bg-yellow-400 rounded-xl absolute left-48 p-2'>Search</button>
+            <div className='w-full flex justify-between items-center px-4 md:px-8'>
+                <div className='relative flex-grow max-w-xs'>
+                    <input type="text" className='bg-white rounded-xl h-10 w-full pl-4 pr-16' placeholder="Search..." />
+                    <button className='bg-yellow-500 rounded-xl absolute right-0 top-0 bottom-0 p-2'>Search</button>
                 </div>
-                <div className='text-white'>
-                    <ul className='flex gap-3 items-center'>
-                        <IoMdHeartEmpty /> <li><Link>wishlist</Link></li>
-                        <BsCart2 />   <li><Link>cart</Link></li>
-                        <div onClick={handleLogout} className='flex items-center mx-10 bg-red-500 p-1 rounded-lg hover:bg-yellow-300 hover:text-black'><button className=''>Logout</button> <RiLogoutBoxRFill /></div>
-                    </ul>
+                <div className='text-white flex items-center space-x-6'>
+                    <div className='hidden md:flex items-center gap-3'>
+                        <IoMdHeartEmpty className='text-xl' />
+                        <li className='list-none'><Link to='/wishlist'>Wishlist</Link></li>
+                    </div>
+                    <div className='hidden md:flex items-center gap-3'>
+                        <BsCart2 className='text-xl' />
+                        <li className='list-none'><Link to='/cart'>Cart</Link></li>
+                    </div>
+                    <div onClick={handleLogout} className='flex items-center bg-red-500 p-2 rounded-lg hover:bg-yellow-300 hover:text-black cursor-pointer  transition-colors duration-300'>
+                        <button className='mr-2'>Logout</button>
+                        <RiLogoutBoxRFill className='text-xl' />
+                    </div>
                 </div>
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
